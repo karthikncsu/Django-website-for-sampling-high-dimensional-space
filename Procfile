4ea70475bbe58137cd01b26d2210f6f3ce1a699c
Karthik Reddy <klyatha@ncsu.edu>
2021-02-16 22:53:43 -0500

    Heroku development files added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ndsample.wsgi --log-file -
--- ndsample/settings.py
+++ ndsample/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!&fv$m1)+wrncf$9*&40b*7ykhppk5vnimklxohqmzm$m(ju^e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
